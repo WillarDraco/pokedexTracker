86c4b6be4d02d2c08eee6905c0c3144f0101752a
willardraco <a1863235@adelaide.edu.au>
2023-10-31 18:26:17 +1030

    Should work

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/WillarDraco/pokemonDB.git
 
 go 1.21.3
+
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
--- main.go
+++ main.go
@@ -1,48 +1,221 @@
 package main
 
-import {
+import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 	"sync"
-	"github.com/WillarDraco/pokemonDB.git"
-	
+
+	"github.com/jcelliott/lumber"
+)
+
+const version = "1.0.0"
+
+type (
+	Logger interface {
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Info(string, ...interface{})
+		Debug(string, ...interface{})
+		Trace(string, ...interface{})
+	}
+
+	Driver struct {
+		mutex   sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir     string
+		log     Logger
+	}
+)
+
+type Options struct {
+	Logger
 }
 
-const version = "1.21.3"
+func New(dir string, options *Options) (*Driver, error) {
+	dir = filepath.Clean(dir)
+	opts := Options{}
+	if options != nil {
+		opts = *options
+	}
+
+	if opts.Logger != nil {
+		opts.Logger = lumber.NewConsoleLogger((lumber.INFO))
+	}
+
+	driver := Driver{
+		dir:     dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log:     opts.Logger,
+	}
+
+	if _, err := os.Stat(dir); err != nil {
+		opts.Logger.Debug("Using '%s' (database already exists)\n", dir)
+		return &driver, nil
+	}
+
+	opts.Logger.Debug("Creating the database at '%s' ...\n", dir)
+	return &driver, os.MkdirAll(dir, 0755)
+}
+
+func (d *Driver) Write(collection string, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record!")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record (no name)!")
+	}
+
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+	fnlPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnlPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+
+	if err != nil {
+		return err
+	}
+
+	b = append(b, byte('\n'))
+
+	if err := os.WriteFile(tmpPath, b, 0644); err != nil {
+		return err
+	}
+
+	return os.Rename(tmpPath, fnlPath)
+}
+
+func (d *Driver) Read(collection string, resource string, v interface{}) error {
+
+	if collection == "" {
+		return fmt.Errorf("Missing collection - unable to read!")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to read!")
+	}
+
+	record := filepath.Join(d.dir, collection, resource)
+
+	if _, err := stat(record); err != nil {
+		return err
+	}
+
+	b, err := os.ReadFile(record + ".json")
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(b, &v)
+}
+
+func (d *Driver) ReadAll(collection string) ([]string, error) {
+
+	if collection == "" {
+		return nil, fmt.Errorf("Missing collection - unable to read!")
+	}
+
+	dir := filepath.Join(d.dir, collection)
+	if _, err := stat(dir); err != nil {
+		return nil, err
+	}
+
+	files, _ := os.ReadDir(dir)
+	var records []string
+
+	for _, file := range files {
+		b, err := os.ReadFile(filepath.Join(dir, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		records = append(records, string(b))
+	}
+
+	return records, nil
+}
+
+func (d *Driver) Delete(collection string, resource string) error {
+	path := filepath.Join(collection, resource)
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, path)
+
+	switch fi, err := stat(dir); {
+	case fi == nil, err != nil:
+		return fmt.Errorf("Unable to find file or directory named %v\n", path)
+	case fi.Mode().IsDir():
+		return os.RemoveAll(dir)
+	case fi.Mode().IsRegular():
+		return os.RemoveAll(dir + ".json")
+	}
+
+	return nil
+}
+
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+	m, ok := d.mutexes[collection]
+
+	if !ok {
+		m = &sync.Mutex{}
+		d.mutexes[collection] = m
+	}
+
+	return m
+}
+
+func stat(path string) (fi os.FileInfo, err error) {
+	if fi, err = os.Stat(path); os.IsNotExist(err) {
+		fi, err = os.Stat(path + "json")
+	}
+	return
+}
 
 type mon struct {
-	id int;
-	name string;
+	id   int
+	name string
 }
 
-func main()  {  
+func main() {
 	dir := "./"
-	db, err := new(dir, nil);
+	db, err := New(dir, nil)
 	if err != nil {
 		fmt.Println("Error ", err)
 	}
 
-	pokemon := []mon {
-		{1, "Bulbasaur"}
-		{2, "Ivysaur"}
-		{3, "Venusaur"}
-		{4, "Charmander"}
-		{5, "Charmeleon"}
-		{6, "Charizard"}
-		{7, "Squirtle"}
-		{8, "Wartortle"}
-		{9, "Blastoise"}
+	pokemon := []mon{
+		{1, "Bulbasaur"},
+		{2, "Ivysaur"},
+		{3, "Venusaur"},
+		{4, "Charmander"},
+		{5, "Charmeleon"},
+		{6, "Charizard"},
+		{7, "Squirtle"},
+		{8, "Wartortle"},
+		{9, "Blastoise"},
 	}
 
-	for _, value := range pokemon{
-		db.write("pokemon", value.id, pokemon{
-			ID: value.id,
+	for _, value := range pokemon {
+		db.Write("pokemon", value.name, mon{
+			ID:   value.id,
 			Name: value.name})
-		}
 	}
 
-	records, err := db.readAll("pokemon")
+	records, err := db.ReadAll("pokemon")
 	if err != nil {
 		fmt.Println("Error", err)
 	}
